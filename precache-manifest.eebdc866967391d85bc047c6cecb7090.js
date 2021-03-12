328591c40d59ea27b09643443d9a3695a3597570
Mehetre <zwnx0r@splm.siemens.com>
2021-03-12 18:14:53 +0530

    Updates

---
+++ precache-manifest.eebdc866967391d85bc047c6cecb7090.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efb6110dc551b1d7e32f188b25d6dd5b",
+    "url": "/Portfolio_using_React/index.html"
+  },
+  {
+    "revision": "4a96c3769ef012e3babc",
+    "url": "/Portfolio_using_React/static/css/main.bda646f8.chunk.css"
+  },
+  {
+    "revision": "4b21fc3694737542bde5",
+    "url": "/Portfolio_using_React/static/js/2.0beb9059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio_using_React/static/js/2.0beb9059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a96c3769ef012e3babc",
+    "url": "/Portfolio_using_React/static/js/main.ce0832d6.chunk.js"
+  },
+  {
+    "revision": "6dad695addee430cabcc",
+    "url": "/Portfolio_using_React/static/js/runtime-main.99383331.js"
+  },
+  {
+    "revision": "4c622e5911bdfefbba592d65b7c56f2b",
+    "url": "/Portfolio_using_React/static/media/dp.4c622e59.png"
+  }
+]);
